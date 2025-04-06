223439d990ed2caf6fd3678b384597251b6cea66
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-04-06 15:37:46 -0300

    :sparkles: save password hashed

--- src/main/java/br/com/lcrodrigues0/todolist/user/UserController.java
+++ src/main/java/br/com/lcrodrigues0/todolist/user/UserController.java
@@ -3,6 +3,9 @@ package br.com.lcrodrigues0.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,6 +32,9 @@ public class UserController {
         if(userFound != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists.");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+        user.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
