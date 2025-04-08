a3b92cc71e145fd32ccd9668d9100ef51b1aa322
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-04-07 21:54:03 -0300

    :sparkles: validate size of the task title

--- src/main/java/br/com/lcrodrigues0/todolist/task/TaskModel.java
+++ src/main/java/br/com/lcrodrigues0/todolist/task/TaskModel.java
@@ -29,4 +29,12 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception{
+        if(title.length() > 50){
+            throw new Exception("The task title should not exceed 50 characters.");
+        }
+
+        this.title = title;
+    }
 }
